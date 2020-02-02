74e80661b63d90d3eebf588bd45879d9590e03ca
sai milind <sai9615@gmail.com>
2020-02-02 03:29:41 -0500

    setup proxy and use concurrently

---
+++ client/src/components/about/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+              Hey wassup  
+            </div>
+        );
+    }
+}
+
+export default index;
